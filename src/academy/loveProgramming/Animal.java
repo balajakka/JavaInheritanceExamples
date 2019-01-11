5f270a12f5b8271d06851f02a37631452ac433f0
balajakka <jbkrao8281@gmail.com>
2019-01-11 19:47:50 +0000

    Its new inhertance project

---
+++ src/academy/loveProgramming/Animal.java
@@ -0,0 +1,10 @@
+package academy.loveProgramming;
+
+/**
+ * Created by balajakka on 11/01/2019.
+ */
+public class Animal {
+    public Animal(){
+        System.out.println("I am in Animal Class");
+    }
+}
---
+++ src/academy/loveProgramming/MyMain.java
@@ -0,0 +1,13 @@
+package academy.loveProgramming;
+
+/**
+ * Created by balajakka on 11/01/2019.
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        GermanShep germShep = new GermanShep();
+        System.out.println("************************************");
+        Cat cat1 = new Cat();
+
+    }
+}
